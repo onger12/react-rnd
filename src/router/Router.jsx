b0cfc54c8242ecfc633016e7c74057b75bf3bb25
dujardin.pizarro <dujardin.pizarro@aldoronline.com>
2024-08-29 14:21:44 -0500

    Agrego enrutamiento para IIS

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'wouter';
+import { Router as RR, Route, Switch } from 'wouter';
 
 import { 
   AboutScreen, AdminScreen, AssigmentsScreen, AuthAdminScreen, AuthColabScreen, CourseScreen, 
@@ -8,25 +8,27 @@ import {
 export const Router = () => {
   return (
     <>
-      <Switch>
-        <Route path="/" component={HomeScreen} />
-        <Route path="/about" component={AboutScreen} />
-        <Route path="/auth/admin" component={AuthAdminScreen} />
-        <Route path="/auth/colab" component={AuthColabScreen} />
+      <RR base="/Capacitaciones-APP">
+        <Switch>
+          <Route path="/" component={HomeScreen} />
+          <Route path="/about" component={AboutScreen} />
+          <Route path="/auth/admin" component={AuthAdminScreen} />
+          <Route path="/auth/colab" component={AuthColabScreen} />
 
-        <Route path="/learn/:dni" component={LearnScreen} />
-        <Route path="/learn/:dni/review" component={ReviewScreen} />
-        <Route path="/learn/:dni/schools" component={SchoolsScreen} />
-        <Route path="/learn/:dni/assigments" component={AssigmentsScreen} />
-        <Route path="/learn/:dni/schools/:schoolId" component={CoursesScreen} />
-        <Route path="/learn/:dni/schools/:schoolId/:courseId" component={CourseScreen} />
-        <Route path="/learn/:dni/schools/:schoolId/:courseId/:videoId" component={CourseScreen} />
+          <Route path="/learn/:dni" component={LearnScreen} />
+          <Route path="/learn/:dni/review" component={ReviewScreen} />
+          <Route path="/learn/:dni/schools" component={SchoolsScreen} />
+          <Route path="/learn/:dni/assigments" component={AssigmentsScreen} />
+          <Route path="/learn/:dni/schools/:schoolId" component={CoursesScreen} />
+          <Route path="/learn/:dni/schools/:schoolId/:courseId" component={CourseScreen} />
+          <Route path="/learn/:dni/schools/:schoolId/:courseId/:videoId" component={CourseScreen} />
 
-        <Route path="/admin" component={AdminScreen} />
-        <Route path="/admin/maker" component={SlidesMakerScreen} />
-        
-        <Route component={NotFoundScreen} />
-      </Switch>
+          <Route path="/admin" component={AdminScreen} />
+          <Route path="/admin/maker" component={SlidesMakerScreen} />
+          
+          <Route component={NotFoundScreen} />
+        </Switch>
+      </RR>
     </>
   )
 }
